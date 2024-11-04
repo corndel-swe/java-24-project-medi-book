802ee6e755a590753edc12c36843a7ec1a42f689
James Kett <james2kett@gmail.com>
2024-11-04 10:34:38 +0000

    Added getalldoctors endpoint

--- src/main/java/org/project/medibook/repositories/DoctorRepository.java
+++ src/main/java/org/project/medibook/repositories/DoctorRepository.java
@@ -1,4 +1,36 @@
 package org.project.medibook.repositories;
 
+import ch.qos.logback.core.model.Model;
+import org.project.medibook.DB;
+import org.project.medibook.model.DoctorModel;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DoctorRepository {
+
+    public static List<DoctorModel> getAllDoctors() throws SQLException {
+        var query = "SELECT * FROM doctors";
+
+        try (var con = DB.getConnection();
+             var stmt = con.createStatement();
+             var rs = stmt.executeQuery(query);) {
+
+            var users = new ArrayList<DoctorModel>();
+            while (rs.next()) {
+                var id = rs.getInt("id");
+                var name = rs.getString("name");
+                var speciality = rs.getString("speciality");
+                var image = rs.getString("image");
+                var biography = rs.getString("biography");
+
+                users.add(new DoctorModel(id, name, speciality, image, biography));
+            }
+
+            return users;
+        }
+    }
 }
