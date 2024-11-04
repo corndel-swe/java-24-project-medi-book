62d0d0d6f69b315e9b579e1dba6673dd57dec78c
Damian <damiangildea12@gmail.com>
2024-11-04 16:46:19 +0000

    Update appointment controller

--- src/main/java/org/project/medibook/controllers/AppointmentController.java
+++ src/main/java/org/project/medibook/controllers/AppointmentController.java
@@ -5,13 +5,18 @@ import org.project.medibook.model.Appointment;
 import org.project.medibook.repositories.AppointmentRepository;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AppointmentController {
   public static void getUpcomingAppointmentsByUserId(Context ctx) throws SQLException {
-    int user_id = ctx.sessionAttribute("userId");
-    List<Appointment> appointments = AppointmentRepository.findUpcomingAppointmentsByUserId(user_id);
+    Map<String, Object> userAttributes = ctx.sessionAttribute("userAttributes");
+
+    Integer userId = (Integer) userAttributes.get("id"); // Adjust type if needed
+    String userName = (String) userAttributes.get("name");
+    String userImage = (String) userAttributes.get("image");
+    List<Appointment> appointments = AppointmentRepository.findUpcomingAppointmentsByUserId(userId);
     ctx.render("user_dashboard", Map.of("appointments", appointments));
   }
 }
