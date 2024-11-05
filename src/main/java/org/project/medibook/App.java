605023c7d190db002cb1a37540a4f8e6dd6c7aae
Damian <damiangildea12@gmail.com>
2024-11-05 09:09:49 +0000

    Updated AJAX

--- src/main/java/org/project/medibook/App.java
+++ src/main/java/org/project/medibook/App.java
@@ -9,7 +9,6 @@ import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
-
 public class App {
     public static void main(String[] args) {
         var javalin = createApp();
@@ -17,56 +16,38 @@ public class App {
     }
 
     public static Javalin createApp() {
-        var app = Javalin.create(
-                config -> {
-                    config.staticFiles.add("src/main/resources/public", Location.EXTERNAL);
+        var app = Javalin.create(config -> {
+            config.staticFiles.add("src/main/resources/public", Location.EXTERNAL);
 
-                    var resolver = new ClassLoaderTemplateResolver();
-                    resolver.setPrefix("/templates/");
-                    resolver.setSuffix(".html");
-                    resolver.setTemplateMode("HTML");
+            var resolver = new ClassLoaderTemplateResolver();
+            resolver.setPrefix("/templates/");
+            resolver.setSuffix(".html");
+            resolver.setTemplateMode("HTML");
 
-                    var engine = new TemplateEngine();
-                    engine.setTemplateResolver(resolver);
+            var engine = new TemplateEngine();
+            engine.setTemplateResolver(resolver);
 
-                    config.fileRenderer(new JavalinThymeleaf(engine));
-                });
+            config.fileRenderer(new JavalinThymeleaf(engine));
+        });
 
-    app.get("/hello", ctx -> {
-        ctx.result("Hello, World!");
-    });
+        app.get("/hello", ctx -> ctx.result("Hello, World!"));
 
+        // USER ENDPOINTS
+        app.get("/", UserController::getAll);
+        app.post("/submit", UserController::userLogIn);
 
-    // USER ENDPOINTS
-    app.get("/", UserController::getAll);
-    app.post("/submit", UserController::userLogIn);
-
-
-
-
-
-    // APPOINTMENT ENDPOINTS
-
-    app.get("/dashboard", AppointmentController::renderDashboard);
-    app.get("/appointment-history", AppointmentController::renderAppointmentHistory);
-    app.get("/appointments/dates", AppointmentController::getAvailableDates);
-    app.get("/appointments/times", AppointmentController::getAvailableTimes);
-    app.get("/appointments/book", AppointmentController::renderBookingPage);
-    app.post("/appointments/book", AppointmentController::bookAppointment);
-
-
-
-
+        // APPOINTMENT ENDPOINTS
+        app.get("/dashboard", AppointmentController::renderDashboard);
+        app.get("/appointment-history", AppointmentController::renderAppointmentHistory);
+        app.get("/appointments/dates", AppointmentController::getAvailableDates);
+        app.get("/appointments/times", AppointmentController::getAvailableTimes);
+        app.get("/appointments/book", AppointmentController::renderBookingPage);
+        app.post("/appointments/book", AppointmentController::bookAppointment);
 
         // DOCTOR ENDPOINTS
         app.get("/doctors/list", DoctorController::getAllDoctorsJson);
         app.get("/doctors", DoctorController::renderDoctors);
         app.get("/doctor/{id}", DoctorController::renderDoctor);
-
-
-
-
-
 
         return app;
     }
